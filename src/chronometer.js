46edee1e141f28f7393cc0640420ac58aae3267e
Yujiro Akihiro <yujiro.akihiro@gmail.com>
2024-04-09 10:46:49 +0200

    #1 Commit

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,18 +1,29 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    if (printTimeCallback) {
+      this.intervalId = setInterval(() => {
+        this.currentTime++;
+        printTimeCallback();
+      }, 1000);
+    } else {
+      this.intervalId = setInterval(() => {
+        this.currentTime++;
+      }, 1000);
+    }
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.
+
   }
 
   computeTwoDigitNumber(value) {
